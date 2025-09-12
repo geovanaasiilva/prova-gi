fda83d6fab1c88c427011e0d6460087b997e411f
gi <geovanaasiilva79@gmail.com>
2025-09-12 08:56:56 -0300

    json

---
+++ BACKEND/backend/logicaProgramacao/atvjson.py
@@ -0,0 +1,12 @@
+import json
+estoque_categoria = []
+estoque_produto = []
+
+id_categoria = 0
+id_produto = 0
+
+def menu():
+    print("1)Cadadastrar categoria")
+    print("2)Cadastrar produto")
+    print("3)Listar produtos")
+    print("4)Sair")
