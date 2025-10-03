ca8d5d4aaa7efed03384d663b854c5a9950287be
gi <geovanaasiilva79@gmail.com>
2025-10-03 11:54:37 -0300

    tarefa

--- gerenciador_web/config/tarefas/urls.py
+++ gerenciador_web/config/tarefas/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views #importa nossas views
 urlpatterns = [
-    path('', views.listar_tarefas , name = 'listar_tarefas') 
+    path('', views.listar_tarefas , name = 'listar_tarefas'),
+    path('<int:tarefa_id>/', views.detalhe_tarefa, name = "detalhe_tarefa"),
+ 
 ]
--- gerenciador_web/config/tarefas/views.py
+++ gerenciador_web/config/tarefas/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
 from .models import Tarefa
-from django.http import HttpResponse
+
 
 def listar_tarefas(request):
+    #1. A busca no banco de dados
     tarefas_salvas = Tarefa.objects.all()
-    print(tarefas_salvas)
-    return HttpResponse("View 'listar_tarefas' executada! Confira no terminal")
+
+    #2. Criamos um "dicionário de contexto" para enviar os dados ao tempate.
+    #3. A chave 'minhas_tarefas' será a variável que usaremos no HTML.
+    contexto = {
+        'minhas_tarefas': tarefas_salvas
+    }
+
+    #3. Renderizamos o template, passando a requisição e o contexto com s dados.
+    return render(request, 'tarefas/lista.html', contexto)
+
+def detalhe_tarefa(request, tarefa_id):
+    #busca uma  tarefa pelo id
+    #se não encontrar retorna erro 404
+    tarefa = get_object_or_404 (Tarefa, pk=tarefa_id)
+    return render (request, 'tarefas/detalhe.html', {'tarefa': tarefa})
